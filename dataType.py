7c9b85269fa37afef57bcdb3f8a640d4bd85a392
unknown <sarahelhayek55@gmail.com>
2024-11-08 23:59:56 +0300

    First commit

---
+++ dataType.py
@@ -0,0 +1,61 @@
+#data type:
+#1-int 2-bool 3-float 4-str 5-list 6-tuple 7-dict 8-complex
+# Q1:
+import math
+#
+# mile = int(input("Enter the mile: "))
+# kilometer = 1.6*mile
+# print(f"{mile} miles is {kilometer} kilometers.")
+
+#Q2:
+# length = float(input("Enter length of the sides and height of equilateral triangle: "))
+# area = (math.sqrt(3)/4)*math.pow(length,2)
+# volume = area*length
+# print(f"the area is {area}")
+# print(f"the volume of the triangular prism is {volume}")
+
+#Q3:
+# meter = int(input("Enter a value for meter: "))
+# feet = 3.2789*meter
+# print(f"{meter} meters is {feet} feet")
+# print("{:5.2f}".format(feet))
+
+#Q4:
+# square = int(input("Enter a number in square meters: "))
+# ping = 0.3025*square
+# print("{:2d} square meters is {:5.3f} pings.".format(square,ping))
+
+#Q5:
+# subtotal = float(input("Enter the subtotal: "))
+# gratuity = float(input("Enter the gratuity: "))
+# rate = float(gratuity/subtotal)
+# total = float(rate+subtotal)
+# total = float(rate+subtotal)
+# print("The gratuity is ${:3.1f} and total is ${:4.1f}".format(rate,total))
+
+#Q6:
+# number = int(input("Enter a number between 0 and 1000: "))
+# digit = number%10
+# num = number // 10
+# dig = num % 10
+# num //= 10
+# d = num%10
+# all = d * digit * dig
+# print(f"digit multiplication of all digits in {number} is {all}.")
+
+#Q7:
+# minutes = int(input("Enter the number of minutes: "))
+# hour = minutes / 60
+# day = minutes //(60*24)
+# year = minutes//(60*24*365)
+# Day = (day - (year*365)) //1
+# print(f"{year} years {Day} days")
+
+#Q9:
+# v0 = float(input("Enter the v0: "))
+# v1 = float(input("Enter the v1: "))
+# t = float(input("Enter the t: "))
+# a = (v1 - v0)/(t)
+# print(a)
+
+#Q10:
